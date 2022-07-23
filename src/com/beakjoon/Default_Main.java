72448150b9422860e39ad3f4ea459ca7c12a6879
croo12 <its19447@gmail.com>
2022-07-24 00:49:44 +0900

    treasure

--- src/com/beakjoon/Default_Main.java
+++ src/com/beakjoon/Default_Main.java
@@ -1,7 +1,12 @@
 package com.beakjoon;
 
 public class Default_Main { // 클래스 이름을 Main으로 바꿔야 한다.
-
+	//이 주의 백준 문제
+	/*
+	1. 보물 (1026)
+	2. 로프 (2217)
+	*/
+	
 	public static void main(String[] args) {
 		
 
