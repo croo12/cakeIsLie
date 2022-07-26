e6e954d3ae0833882aa8f4097c023c9795afd496
its19447 <its19447@gmail.com>
2022-07-26 17:32:50 +0900

    failed....

---
+++ src/com/beakjoon/ssafy/Tomato.java
@@ -0,0 +1,87 @@
+package com.beakjoon.ssafy;
+
+import java.util.Scanner;
+
+public class Tomato {
+	
+	public static int end = 0;
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int[][] box = new int[x][y];
+		
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				box[i][j] = sc.nextInt(); //1은 익은 토마토 , 2는 안익은 토마토 
+			}
+		}
+		int result = 0;
+		while(true) {
+			result++;
+			box = day_infection(box);
+			if( box == null) {
+				result=-1;
+				break;
+			}else if(end == 1) {
+				break;
+			}
+		}
+		System.out.println(result);
+		sc.close();
+	}
+	
+	public static int[][] today_rotten_tomato_checker(int[][] box) { //오늘 썩을 토마토는 어디에?
+		int[] dx = {1, 0, 0, -1};
+		int[] dy = {0, 1, -1, 0};
+
+		int[][] tmp_box = new int[box.length][box[0].length];
+		
+		for (int i = 0; i < box.length; i++) {
+			for (int j = 0; j < box[0].length; j++) {
+				if(box[i][j] == 1)
+					for (int m = 0; m < 4; m++) {
+						int next_x = i+dx[m];
+						int next_y = j+dy[m];
+						if(next_x <0 || next_x >= box.length || next_y <0 || next_y >= box[0].length) {
+							
+						}else{
+							tmp_box[next_x][next_y] = 1;
+						}
+					}
+			}
+		}
+		return tmp_box;
+	}
+	
+	public static int[][] day_infection(int[][] box){
+		
+		int[][] tmp_box = today_rotten_tomato_checker(box);
+		
+		int cnt =0; ////오늘 썩은 토마토의 갯수 계산용
+		for (int i = 0; i < box.length; i++) { // 오늘 썩을 토마토 상자를 실제 토마토 상자에 반영함
+			for (int j = 0; j < box[0].length; j++) {
+				if(tmp_box[i][j] == 1 && box[i][j] == 0) {
+					box[i][j] = 1;
+					cnt++;
+				}
+			}
+		}
+		if(is_all_rotten(box)) {
+			end = 1;
+			return box;
+		}
+		if(cnt == 0) return null; //더이상 썩지 않음
+		else return box;
+	}
+	
+	public static boolean is_all_rotten(int[][] box) {
+		for (int i = 0; i < box.length; i++) {
+			for (int j = 0; j < box[0].length; j++) {
+				if(box[i][j] == 0) return false;
+			}
+		}
+		return true;
+	}
+}
