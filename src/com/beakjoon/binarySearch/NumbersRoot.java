e45d514c396428fe71b005c5cd08162a336d77fe
croo12 <its19447@gmail.com>
2022-08-03 01:27:56 +0900

    al

---
+++ src/com/beakjoon/binarySearch/NumbersRoot.java
@@ -0,0 +1,15 @@
+package com.beakjoon.binarySearch;
+
+import java.util.Scanner;
+
+public class NumbersRoot {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int start = 1;
+		int end = n;
+		int mid = (start + end)/2;
+	}
+}
