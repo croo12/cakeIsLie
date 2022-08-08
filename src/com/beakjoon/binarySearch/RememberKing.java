580ffb10e27c1ca0a800a93abdc778b4ab7b9d26
croo12 <its19447@gmail.com>
2022-08-08 22:26:39 +0900

    good

---
+++ src/com/beakjoon/binarySearch/RememberKing.java
@@ -0,0 +1,55 @@
+package com.beakjoon.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class RememberKing {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int a = Integer.parseInt(br.readLine());
+			int[] aar = new int[a];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < a; i++) {
+				aar[i] = Integer.parseInt(st.nextToken());
+			}
+			int b = Integer.parseInt(br.readLine());
+			int[] bar = new int[b];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < b; i++) {
+				bar[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(aar);
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < bar.length; i++) {
+				int ans = bar[i];
+				int start = 0;
+				int end = a-1;
+				int mid = (start + end)/2;
+				while(end >= start) {
+					if(ans > aar[mid]) {
+						start = mid+1;
+					}else if(ans < aar[mid]) {
+						end = mid - 1;
+					}else {
+						break;
+					}
+					mid = (start+end)/2;
+				}
+				if(ans != aar[mid]) sb.append("0\n");
+				else sb.append("1\n");
+			}
+			System.out.print(sb);
+		}
+	}
+}
---
+++ src/com/beakjoon/binarySearch/TheLiquid.java
@@ -0,0 +1,83 @@
+package com.beakjoon.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class TheLiquid {
+	
+	static int finalResult = 2000000001;
+	static int finalA = 0;
+	static int finalB = 0;
+	
+	static int[] arr;
+	static int n;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+//		Arrays.sort(arr);
+		searchValue();
+		
+		System.out.println(arr[finalA] + " " + arr[finalB]);
+	}
+	
+	private static int absoluteValue(int value) {
+		if(value >= 0) return value;
+		else return -1 * value;
+	}
+	
+	private static void searchValue() {
+		for (int i = 0; i < n; i++) {
+//			System.out.println(i+"번 가동");
+			int ans = arr[i];
+			
+			int start = i+1;
+			int end = n-1;
+			int mid = (start + end)/2;
+			
+			int result = 2000000001;
+			int tmpA = 0;
+			int tmpB = 0;
+			
+			while(end >= start) {
+				int sum = ans + arr[mid];
+				int av = absoluteValue(sum);
+				if(sum > 0 ) {
+					end = mid -1;
+					if(result > av) {
+						result = av;
+						tmpA = i;
+						tmpB = mid;
+					} 
+				}else if(sum < 0) {
+					start = mid +1;
+					if(result > av) {
+						result = av;
+						tmpA = i;
+						tmpB = mid;
+					} 
+				}else {
+					finalA = i;
+					finalB = mid;
+					finalResult = av;
+					return;
+				}
+				mid = (start + end)/2;
+			}
+			
+			if(result < finalResult) {
+				finalResult = result;
+				finalA = tmpA;
+				finalB = tmpB;
+			}
+		}//for
+	}
+}
