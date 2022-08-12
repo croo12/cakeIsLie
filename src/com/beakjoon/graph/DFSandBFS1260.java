42d137018217358d3e447cff6748edd27a5658f3
croo12 <its19447@gmail.com>
2022-08-13 00:48:15 +0900

    my dfs have problem

---
+++ src/com/beakjoon/graph/DFSandBFS1260.java
@@ -0,0 +1,108 @@
+package com.beakjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class DFSandBFS1260 {
+	
+	static HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		int m = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			HashSet<Integer> tmp1 = map.get(num1);
+			HashSet<Integer> tmp2 = map.get(num2);
+			
+			if(tmp1 == null) tmp1 = new HashSet<>();
+			if(tmp2 == null) tmp2 = new HashSet<>();
+			
+			tmp1.add(num2);
+			tmp2.add(num1);
+			
+			map.put(num1, tmp1);
+			map.put(num2, tmp2);
+		}
+		
+		doDFS(start);
+		System.out.println();
+		doBFS(start);
+		System.out.println();
+	}
+	
+	private static void doDFS(int start) { //dfs의 출력에 문제가 있다 호호호
+		boolean[] visited = new boolean[N+1];
+		visited[start] = true;
+		
+		Stack<Integer> stk = new Stack<>();
+		stk.push(start);
+		
+		
+		
+		while(!stk.isEmpty()) {
+			int now = stk.peek();
+			HashSet<Integer> tmp = map.get(now);
+			
+			boolean newChallenger = false;
+			
+			Iterator<Integer> iter = tmp.iterator();
+			while(iter.hasNext()) {
+				int next = iter.next();
+				if(!visited[next]) {
+					stk.push(next);
+					visited[next] = true;
+					newChallenger = true;
+					
+					break;
+				}
+			}
+			
+			if(!newChallenger) {
+				stk.pop();
+				System.out.printf("%d ",now);
+			}
+		}
+	}
+	
+	private static void doBFS(int start) {
+		boolean[] visited = new boolean[N+1];
+		visited[start] = true;
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(start);
+		
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			HashSet<Integer> tmp = map.get(now);
+			
+			Iterator<Integer> iter = tmp.iterator();
+			while(iter.hasNext()) {
+				int next = iter.next();
+				if(!visited[next]) {
+					q.add(next);
+					visited[next] = true;
+				}
+			}
+			System.out.printf("%d ",now);
+		}
+	}
+}
--- src/com/beakjoon/graph/Tomato7576.java
+++ src/com/beakjoon/graph/Tomato7576.java
@@ -3,6 +3,8 @@ package com.beakjoon.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Tomato7576 {
@@ -10,10 +12,9 @@ public class Tomato7576 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int freshT = 0;
 	
-	static boolean[][] todayRotted;
+	static Queue<PairXY> q = new LinkedList<>();
+	
 	static int[][] box;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
 	
 	static int width;
 	static int height;
@@ -23,77 +24,66 @@ public class Tomato7576 {
 		width = Integer.parseInt(st.nextToken());
 		height = Integer.parseInt(st.nextToken());
 		box = new int[height][width];
+//		boolean[][] visited = new boolean[height][width];
+		
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
 				box[i][j] = readInt();
-			}
-		}
-
-//		for(int[] a : box) {
-//			for(int i : a) System.out.print(i+" ");
-//			System.out.println();
-//		}
-		
-//========================입력부====================================
-		
-		
-		
-		int day = 0;
-		
-		
-		while(freshT>0) {
-			todayRotted = new boolean[height][width];
-			day++;
-			//썩은 사과를 찾는다
-			//썩은 사과의 상하좌우를 찾는다.
-			int rotten = search();
-			freshT -= rotten;
-			if(freshT == 0) {
-				break;
-			}
-			if(rotten == 0) {
-				day = -1;
-				break;
-			}
-			
-			
-//			System.out.printf("남은 새 도마도 %d개\n",freshT);
-			
-			//몇개가 썩었는지 센다.
-			
-		}
-		System.out.println(day);
-	}
-	
-	private static int search() {
-		
-		int todayCnt = 0;
-		
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				
-//				System.out.printf(
-//						"안녕하세요 도마도(%d,%d) %b, 상자의 도마도 = %d\n",j,i,todayRotted[i][j], box[i][j]);
-				
-				if(todayRotted[i][j]) continue;
-				
 				if(box[i][j] == 1) {
-					for (int k = 0; k < 4; k++) {
-						int nextX = j+dx[k];
-						int nextY = i+dy[k];
-						
-						if(nextX < 0 || nextX >= width || nextY < 0 || nextY >= height) continue;
-						else if((!todayRotted[nextY][nextX]) && box[nextY][nextX] == 0){
-							todayRotted[nextY][nextX] = true;
-							box[nextY][nextX] = 1;
-							todayCnt++;
-						}
-					}
+					q.add(new PairXY(j, i));
 				}
 			}
 		}
-//		System.out.printf("잘 끝났어요 todayCnt , %d\n",todayCnt);
-		return todayCnt;
+		
+		int[] dx = {1,-1,0,0};
+		int[] dy = {0,0,1,-1};
+		if (freshT == 0) {
+			System.out.println(0);
+		}else {
+			while(!q.isEmpty()) {
+				PairXY now = q.poll();
+				
+	//			visited[now.y][now.x] = true;
+	//			!visited[nextY][nextX] && 			
+				
+				for (int k = 0; k < 4; k++) {
+					int nextX = now.x + dx[k];
+					int nextY = now.y + dy[k];
+					if(nextX >= width || nextY >= height || nextX < 0 || nextY <0) {
+						continue;
+					}
+					if( box[nextY][nextX] == 0) {
+						box[nextY][nextX] = box[now.y][now.x] + 1;
+						q.add(new PairXY(nextX, nextY));
+						freshT--;
+					}
+				}
+			}
+			if(freshT > 0) {
+				System.out.println(-1);
+			}else {
+				last();
+			}
+		}
+		
+//		for(int[] a : box) {
+//			for(int i : a) System.out.printf("%d ",i);
+//			System.out.println();
+//		}
+//		System.out.printf("남은 과일 %d",freshT);
+		
+	}
+	
+	private static void last() {
+		int max = 0;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if(box[i][j] > max) {
+					max = box[i][j];
+				}
+			}
+		}
+		System.out.println(max-1);
 	}
 	
 	private static int readInt() throws IOException {
@@ -108,5 +98,14 @@ public class Tomato7576 {
 			}else if(now == '1')
 				return 1;
 		}
+	}
+}
+
+class PairXY {
+	int x;
+	int y;
+	public PairXY(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 }
