581bf43c7be1d2f8f3f69e8e67fa86aed6a1b8ea
croo12 <its19447@gmail.com>
2022-08-17 22:32:44 +0900

    tree complete

---
+++ src/com/beakjoon/class1/CheckNumber.java
@@ -0,0 +1,18 @@
+package com.beakjoon.class1;
+
+import java.util.Scanner;
+
+public class CheckNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum= 0;
+		
+		for (int i = 0; i < 5; i++) {
+			int now = sc.nextInt();
+			sum += now*now;
+		}
+		
+		System.out.println(sum%10);
+		sc.close();
+	}
+}
