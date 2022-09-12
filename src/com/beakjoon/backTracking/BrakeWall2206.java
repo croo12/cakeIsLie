e9203db87b793f7b634f13ea1472eef4282f0982
croo12 <its19447@gmail.com>
2022-09-13 02:03:16 +0900

    hkk

---
+++ src/com/beakjoon/backTracking/BrakeWall2206.java
@@ -0,0 +1,97 @@
+package com.beakjoon.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BrakeWall2206 {
+	
+	static int[][] arr;
+	static boolean[][] vis;
+	static int[] dx = {1,-1,0,0};
+	static int[] dy = {0,0,1,-1};
+	static int min = 1000000;
+	
+	static int N;
+	static int M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr =new int[N][M];
+		List<Pair> walls = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = s.charAt(j) - '0';
+				if(arr[i][j] == 1) {
+					walls.add(new Pair(j,i,0));
+				}
+			}
+		}
+		
+		Pair now;
+		if(walls.size() != 0)
+			for (int i = 0; i < walls.size(); i++) {
+				vis = new boolean[N][M];
+				now = walls.get(i);
+				arr[now.y][now.x] = 0;
+				bfs();
+				arr[now.y][now.x] = 1;
+			}
+		else
+			min = N+M-1;
+		
+		if(min == 1000000) System.out.println(-1);
+		else System.out.println(min);
+	}
+	
+	static void bfs() {
+		Queue<Pair> q = new LinkedList<>();
+		q.offer(new Pair(0,0,1));
+		vis[0][0] = true;
+		
+		Pair now;
+		while(!q.isEmpty()) {
+			now = q.poll();
+			
+			if(now.x == M-1 && now.y == N-1) {
+				min = Math.min(now.num, min);
+			}
+			
+			if(now.num >= min) return;
+			
+			for (int k = 0; k < 4; k++) {
+				int rx = now.x + dx[k];
+				int ry = now.y + dy[k];
+				
+				if(rx >= M || ry >= N || rx < 0 || ry < 0) continue;
+				if(!vis[ry][rx] && arr[ry][rx] == 0) {
+					vis[ry][rx] = true;
+					q.offer(new Pair(rx,ry,now.num+1));
+				}
+			}
+		}
+		
+	}
+	
+	static class Pair{
+		int x;
+		int y;
+		int num;
+		Pair(int x, int y, int num){
+			this.x = x;
+			this.y = y;
+			this.num = num;
+		}
+	}
+}
--- src/com/beakjoon/backTracking/OddPrimeNumber2023.java
+++ src/com/beakjoon/backTracking/OddPrimeNumber2023.java
@@ -5,24 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class OddPrimeNumber2023 {
-	static boolean[] notPn;
 	static int N;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		int MAX = (int)Math.pow(10, N);
-		notPn = new boolean[MAX];
-		notPn[0] = true; notPn[1] = true;
-		
-		for (int i = 2; i < MAX; i++)
-			if(!notPn[i])
-				for (int j = i*i; j < MAX; j+=i)
-					notPn[j] = true;
-		
+//		int MAX = (int)Math.pow(10, N);
+//		int idx = 1;
+//		int[] cnt = new int[N];
+//		while(idx > 0) {
+//			if(idx == N) {
+//				int last = cnt[N-1]*10;
+//				for (int i = last; i < last + 10; i++) {
+//					if(check(i)) {
+////						notPn[i] = true;
+//						sb.append(i).append('\n');
+//						
+//					}
+//				}
+//				idx--;
+//			}
+//			int before = cnt[idx-1];
+//			boolean flag = false;
+//			for (int i = before*10; i < (before*10) + 10; i++) {
+//				if(check(i)) {
+////					System.out.printf("지금 서순 'idx : %d' , 지금 고른 수 ' %d' \n",idx,i);
+//					cnt[idx] = i;
+//					idx++;
+//					flag = true;
+//					break;
+//				}
+//			}
+//			if(!flag)
+//				idx--;
+//		}
 		dfs(0,0);
 		
 		System.out.print(sb);
+	}
+	
+	private static boolean check(int num) {
+		if(num == 0 || num == 1)return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if(num % i == 0)
+				return false;
+		}
+		return true;
 	}
 	
 	private static void dfs(int num, int depth) {
@@ -32,8 +60,9 @@ public class OddPrimeNumber2023 {
 		}
 		
 		for (int i = num; i < num+10; i++) {
-			if(!notPn[i])
+			if(check(i)) {
 				dfs(i*10,depth+1);
+			}
 		}
 		
 	}
